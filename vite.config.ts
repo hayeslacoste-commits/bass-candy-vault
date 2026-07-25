cf2797ae603e7e5214c55eedfd851ac6a662df9b
hayeslacoste-commits <hayeslacoste@gmail.com>
2026-07-25 16:59:23 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
-import lovableViteConfig from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  ...lovableViteConfig,
+  tanstackStart: {
+    server: {
+      entry: "server"
+    }
+  }
 });
