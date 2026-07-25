e3af6be328facaeab42c8e9a7e3bfb7270dcc8ed
hayeslacoste-commits <hayeslacoste@gmail.com>
2026-07-24 22:34:20 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,14 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
     server: { entry: "server" },
   },
+  html: {
+    transformIndexHtml(html) {
+      return html.replace(
+        /<link rel="icon"[^>]*>/g,
+        `<link rel="icon" type="image/x-icon" href="https://i.postimg.cc/cCNwnbrh/favicon.jpg" />`
+      );
+    }
+  }
 });
