d248861a9b6e73d07250e768213d139f6497de0d
hayeslacoste-commits <hayeslacoste@gmail.com>
2026-07-25 16:30:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [],
-  build: {
-    emptyOutDir: true,
-  }
+  tanstackStart: {
+    server: { entry: "server" },
+  },
 });
