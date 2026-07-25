2c57b2aa6619b009e39f7d071b784677e1e99882
hayeslacoste-commits <hayeslacoste@gmail.com>
2026-07-25 09:13:00 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     transformIndexHtml(html) {
       return html.replace(
         /<link rel="icon"[^>]*>/g,
-        `<link rel="icon" type="image/x-icon" href="https://i.postimg.cc/cCNwnbrh/favicon.jpg" />`
+        `<link rel="icon" type="image/x-icon" href="https://i.postimg.cc/cCNwnbrh/favicon.jpg?v=2" />`
       );
     }
   }
