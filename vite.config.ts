46a6f9259ab498203fd83d24f5e9533f65e5e7a9
hayeslacoste-commits <hayeslacoste@gmail.com>
2026-07-25 13:17:35 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  html: {
-    transformIndexHtml(html) {
-      return html.replace(
-        /<\/head>/,
-        `<script>
-          const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-          link.type = 'image/x-icon';
-          link.rel = 'shortcut icon';
-          link.href = '/favicon.ico';
-          document.getElementsByTagName('head')[0].appendChild(link);
-        </script></head>`
-      );
-    }
-  }
 });
