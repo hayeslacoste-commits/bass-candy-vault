b1d76ab92ae33432e57556c2cf2aef5667f1ab4c
hayeslacoste-commits <hayeslacoste@gmail.com>
2026-07-26 10:27:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: {
-        preset: "vercel", 
-      },
+        entry: "server"
+      }
     }),
-    tailwindcss(),
-  ],
+    nitro({
+      preset: "vercel"
+    }),
+    tailwindcss()
+  ]
 });
